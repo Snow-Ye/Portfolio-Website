0f209680b068847c52b703db08c115d99bb5df49
Steven Ye <86841748+Snow-Ye@users.noreply.github.com>
2023-10-25 05:57:56 +0000

    Oct25

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return 
+  return (
     <main className="">
-      
+      <div><p>HIHI</p></div>
     </main>
-  
+  )
 }
---
+++ components/header.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from 'react'
+import { motion } from "framer-motion";
+import { links } from "@/lib/data";
+import Link from "next/link";
+import clsx from "clsx";
+import { useActiveSectionContext } from "@/context/active-section-context";
+
+// 两个部分 一个是背景部分 一个是内容nav bar  因为特效不同要分别渲染
+
+export default function header() {
+  return (
+    <header className = "z-[999] relative">
+        <motion.div
+        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full "
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+      ></motion.div>
+
+
+
+
+    </header>
+  )
+}
